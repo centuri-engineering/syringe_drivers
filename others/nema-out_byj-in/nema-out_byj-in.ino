19257fea092fdae056d695a8a99f15fbade4d9dd
Hervé Rouault <herve.rouault@univ-amu.fr>
2022-04-22 11:21:29 +0200

    correct race condition in the in_out version
    * sort the headers
    * remove the flush function
    * use _in _out names for the stepper instances
    * better serial port parsing

--- others/nema-out_byj-in/nema-out_byj-in.ino
+++ others/nema-out_byj-in/nema-out_byj-in.ino
@@ -1,80 +1,70 @@
 /*
-   Code adapted from BasicStepperDriver example from the StepperDriver library. Copyright (C)2015-2017 Laurentiu Badea
-   Simple demo, should work with any driver board
-   Connect STEP, DIR as indicated
-   This file may be redistributed under the terms of the MIT license.
-   A copy of this license has been included with this distribution in the file LICENSE.
+   Code adapted from BasicStepperDriver example from the StepperDriver library.
+   Copyright (C)2015-2017 Laurentiu Badea Simple demo, should work with any
+   driver board Connect STEP, DIR as indicated
+
+   Code adapted from stepper_oneRevolution from the Stepper library, Tom Igoe
+
+   This file may be redistributed
+   under the terms of the MIT license. A copy of this license has been included
+   with this distribution in the file LICENSE.
 
 */
 #include <Arduino.h>
+#include <Stepper.h>
+
 #include "BasicStepperDriver.h"
 
+// Initialization of the Nema driver (reward delivery)
 // Motor steps per revolution. Most steppers are 200 steps or 1.8 degrees/step
 #define MOTOR_STEPS 200
 #define RPM 10
-// Since microstepping is set externally, make sure this matches the selected mode
-// If it doesn't, the motor will move at a different RPM than chosen
-// 1=full step, 2=half step etc.
+// Since microstepping is set externally, make sure this matches the selected
+// mode If it doesn't, the motor will move at a different RPM than chosen 1=full
+// step, 2=half step etc.
 #define MICROSTEPS 16
 
 // All the wires needed for full functionality
 #define DIR 8
 #define STEP 9
-//Uncomment line to use enable/disable functionality
+// Uncomment line to use enable/disable functionality
 //#define SLEEP 13
 
 // 2-wire basic config, microstepping is hardwired on the driver
-BasicStepperDriver stepper2(MOTOR_STEPS, DIR, STEP);
+BasicStepperDriver stepper_out(MOTOR_STEPS, DIR, STEP);
 
-//Uncomment line to use enable/disable functionality
-//BasicStepperDriver stepper(MOTOR_STEPS, DIR, STEP, SLEEP);
-
-bool fullRotation;
-
-/*
- * Code adapted from stepper_oneRevolution from the Stepper library, Tom Igoe
- */
-#include <Stepper.h>
-
+// Initialization of the byj driver (reward sucking)
 // change this to the number of steps on your motor
-#define STEPS 2048
+#define STEPS_IN 2048
 
 // create an instance of the stepper class, specifying
 // the number of steps of the motor and the pins it's
 // attached to
-Stepper stepper(STEPS, 4, 6, 5, 7);
-long steps;
-String letter;
+Stepper stepper_in(STEPS_IN, 4, 6, 5, 7);
 
 void setup() {
-  stepper.setSpeed(10);
-  stepper2.begin(RPM, MICROSTEPS);
+  stepper_in.setSpeed(10);
+  stepper_out.begin(RPM, MICROSTEPS);
   Serial.begin(9600);
 }
 
 void loop() {
   if (Serial.available()) {
-    letter = Serial.read();
-    steps = Serial.parseInt();
-    Serial.print("Letter is :");
-    Serial.println(letter);
-    Serial.print("Step is : ");
-    Serial.println(steps);
-    serialFlush();
-        if (fullRotation == true) {
-      steps = steps * MICROSTEPS * MOTOR_STEPS;
-    }
-    if (letter == "122"){//type "z"
-      stepper.step(steps);
-    }
-    else if (letter == "97"){ //type "a"
-      stepper2.move(-steps);
+    int letter = Serial.read();
+    if (letter == 'z') { // Sucking reward
+      long steps = Serial.parseInt();
+      Serial.print("Sucking reward with ");
+      Serial.print(steps);
+      Serial.println(" steps");
+      stepper_in.step(steps);
+    } else if (letter == 'a') {
+      long steps = Serial.parseInt();
+      Serial.print("Delivering reward with ");
+      Serial.print(steps);
+      Serial.println(" steps");
+      stepper_out.move(-steps);
+    } else {
+      Serial.print("Command not recognized: ");
+      Serial.println(letter);
     }
   }
-}
-
-void serialFlush(){
-  while(Serial.available() > 0) {
-    char t = Serial.read();
-  }
-}
